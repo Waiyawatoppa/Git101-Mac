b1246ad007520905410977ffa1a1d41f0a9dd56f
Waiyawat Aphiraktanon <waiyawat.a@ku.th>
2025-01-31 03:18:43 +0700

    Update Hello Git in C and Hello Git in C++

--- code-01.c
+++ code-01.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    char text[] = "Hello Git";
+    char text[] = "Hello Git in C";
     printf("%s\n", text);
     return 0;
 }
--- code-02.cpp
+++ code-02.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::string text = "Hello Git in C++";
-    std::cout << text << std::endl;
+    std::cout << "Hello Git in C++" << std::endl;
     return 0;
 }
